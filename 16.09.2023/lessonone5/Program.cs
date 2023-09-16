f173d0ac3b32de6ea31cae0c8a179ad510bd3009
chemistbazalt <chemistbazalt@gmail.com>
2023-09-16 15:04:59 +0300

    Написана программа для нахождения самой тяжелой гири

--- 16.09.2023/lessonone5/Program.cs
+++ 16.09.2023/lessonone5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Набор гирь с разным весом
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+// Изначально представим, что гиря "а" имеет максимальный вес
+int max = a;
+
+//если значение первой гири больше, чем макимум, то в максимум нужно положить новое значение
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e; 
+
+//показать на экране максимальную гирю
+Console.Write("Max = ");
+Console.WriteLine(max);
